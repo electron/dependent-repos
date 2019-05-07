51643cf226c1125e31122b2c12802a56d69d3832
Shelley Vohr <shelley.vohr@gmail.com>
2019-05-07 15:27:06 -0700

    chore: convert to use GitHub Actions

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x            # print commands before execution
+set -o errexit    # always exit on error
+set -o pipefail   # honor exit codes when piping
+set -o nounset    # fail on unset variables
+
+npm run build
+
+[[ `git status --porcelain` ]] || exit
+
+git add .
+git config user.email "electron@github.com"
+git config user.name "Electron Bot"
+git commit -am "chore: update dependent-repos database"
+
+git push origin master --follow-tags
